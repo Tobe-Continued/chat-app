ea6cf3b3048990bec81e6dc2c69858bb7393e697
Tobe-Continued <do.you.continue@gmail.com>
2020-09-23 17:14:20 +0200

    React 03

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
-    const online = "Online";
-    const offline = "Offline";
   return (
     <div className="App">
-      <Contact avatar={"https://randomuser.me/api/portraits/men/43.jpg"} name={"Rafael Watson"} isOnline={true} online={online} offline={offline}/>
-      <Contact avatar={"https://randomuser.me/api/portraits/men/45.jpg"} name={"Benjamin Holt"} isOnline={false} online={online} offline={offline}/>
-      <Contact avatar={"https://randomuser.me/api/portraits/women/13.jpg"} name={"Gwendolyn Wade"} isOnline={true} online={online} offline={offline}/>
+        <ContactList/>
     </div>
   );
 }
